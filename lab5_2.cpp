f7054161272d6b30bb0146dd7b21d999e6dea040
nayrunner <Nutchanon_thonganun@cmu.co.th>
2020-01-07 16:02:29 +0700

    Enter

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
+string mixText(string x ,string y)
+{
+	if(x.size() != y.size()){
+	return"E" ;}
+	else{
+		int i=0;
+		string s;
+		while(i< x.size()){
+			s= s+ x[i]+y[i];
+			i++;
+		}
+		return s;
+	}
+}
 
 
 
